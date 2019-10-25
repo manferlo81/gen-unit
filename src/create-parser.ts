b6497110fe2d9c5b7584e4f00a56d7efbca092a8
Manuel Fernandez <manferlo81@gmail.com>
2019-10-25 13:26:48 +0700

    feat(parse): lowercase kilo & negative support

--- src/create-parser.ts
+++ src/create-parser.ts
@@ -4,12 +4,27 @@ const table = [
   { pre: "n", power: -9 },
   { pre: "u", power: -6 },
   { pre: "m", power: -3 },
+  { pre: "k", power: 3 },
   { pre: "K", power: 3 },
   { pre: "M", power: 6 },
   { pre: "meg", power: 6 },
   { pre: "G", power: 9 },
   { pre: "T", power: 12 },
 ].sort((a, b) => b.pre.length - a.pre.length);
+
+// const table2 = {
+//   f: -15,
+//   p: -12,
+//   n: -9,
+//   u: -6,
+//   m: -3,
+//   k: 3,
+//   K: 3,
+//   M: 6,
+//   meg: 6,
+//   G: 9,
+//   T: 12,
+// };
 
 function jjj(val: number, unit: string, base?: string) {
 
@@ -41,7 +56,7 @@ export function createParser({ unit: base }: CreateParserOptions) {
       return asNum;
     }
 
-    const result = /\s*(\d+\.?\d*)\s*(\w*)\s*/.exec(`${input}`);
+    const result = /^\s*(\-?\d+\.?\d*)\s*(\w*)\s*$/.exec(`${input}`);
 
     if (!result) {
       return null;
