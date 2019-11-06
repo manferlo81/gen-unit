72b1338b2ccbd6f836a3b96cdbc948674ee13f60
Manuel Fernandez <manferlo81@gmail.com>
2019-11-06 18:43:52 +1100

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 module.exports = {
 
-  testEnvironment: "node",
+  testEnvironment: 'node',
   browser: false,
 
-  preset: "ts-jest",
-  cacheDirectory: "node_modules/.cache/jest",
+  preset: 'ts-jest',
+  cacheDirectory: 'node_modules/.cache/jest',
 
   collectCoverage: true,
   collectCoverageFrom: [
-    "src/**/*.ts"
+    'src/**/*.ts'
   ],
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
   coverageReporters: process.env.CI
-    ? ["json", "clover"]
-    : ["lcov", "text", "text-summary"],
+    ? ['json', 'clover']
+    : ['lcov', 'text', 'text-summary'],
 
   verbose: true,
 
-};
+}
