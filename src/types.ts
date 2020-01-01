c32656f9b1f1df63833fcdbafe78d46fbe0dbe2d
Manuel Fernandez <manferlo81@gmail.com>
2020-01-02 05:06:41 +0700

    chore: table option deprecated

--- src/types.ts
+++ src/types.ts
@@ -8,28 +8,27 @@ export interface RoundOptions {
   fixed?: boolean;
 }
 
-export type RoundNumberFunction = (num: number) => string | number;
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface DeprecatedCreateParserOptions { }
+interface DeprecatedCreateParserOptions {
+  table?: TableItem[];
+}
 
 export interface CreateParserOptions extends DeprecatedCreateParserOptions {
   unit?: string;
-  table?: TableItem[];
 }
 
 interface DeprecatedCreateFormatterOptions {
   dec?: number | string;
   fixed?: boolean;
+  table?: TableItem[];
 }
 
 export interface CreateFormatterOptions extends DeprecatedCreateFormatterOptions {
   unit?: string;
-  table?: TableItem[];
   round?: RoundOptions | RoundNumberFunction;
 }
 
 export type FindUnitFunction = (value: number) => ({ div: number; pre: string })
+export type RoundNumberFunction = (num: number) => (string | number);
 
 export type ParseFunction = (input: string | number | object) => (number | null)
 export type FormatFunction = (value: number) => string
