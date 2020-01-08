9bd9c8bc875474802441341f521f5e06c36f4d54
Manuel Fernandez <manferlo81@gmail.com>
2020-01-08 14:24:30 +0700

    chore: unit finder cleanup

--- src/create-unit-finder.ts
+++ src/create-unit-finder.ts
@@ -2,12 +2,12 @@ import { FindUnitFunction, FindUnitResult } from './formatter-types'
 import { pow } from './math'
 import { TableItem } from './types'
 
-function createModernUnitFinder(table: FindUnitResult[]): FindUnitFunction {
+function createModernUnitFinder(table: FindUnitResult[], unity: FindUnitResult): FindUnitFunction {
 
   return (value: number): FindUnitResult => {
 
     if (!value) {
-      return { pre: '', div: 1 }
+      return unity
     }
 
     const last = table.length - 1
@@ -26,25 +26,31 @@ function createModernUnitFinder(table: FindUnitResult[]): FindUnitFunction {
 
 function createUnitFinder(table?: TableItem[]): FindUnitFunction {
 
+  const unity = { pre: '', div: 1 }
+
   if (!table) {
-    return createModernUnitFinder([
-      { pre: 'T', div: 1e12 },
-      { pre: 'G', div: 1e9 },
-      { pre: 'M', div: 1e6 },
-      { pre: 'K', div: 1e3 },
-      { pre: '', div: 1 },
-      { pre: 'm', div: 1e-3 },
-      { pre: '\u00b5', div: 1e-6 },
-      { pre: 'n', div: 1e-9 },
-      { pre: 'p', div: 1e-12 },
-      { pre: 'f', div: 1e-15 },
-    ])
+    return createModernUnitFinder(
+      [
+        { pre: 'T', div: 1e12 },
+        { pre: 'G', div: 1e9 },
+        { pre: 'M', div: 1e6 },
+        { pre: 'K', div: 1e3 },
+        unity,
+        { pre: 'm', div: 1e-3 },
+        { pre: '\u00b5', div: 1e-6 },
+        { pre: 'n', div: 1e-9 },
+        { pre: 'p', div: 1e-12 },
+        { pre: 'f', div: 1e-15 },
+      ],
+      unity,
+    )
   }
 
   return createModernUnitFinder(
     table.map<FindUnitResult>(
       ({ pre, power }) => ({ pre, div: pow(10, power) }),
-    ).sort(({ div: div1 }, { div: div2 }) => (div2 - div1)),
+    ).sort((a, b) => (b.div - a.div)),
+    unity,
   )
 
 }
