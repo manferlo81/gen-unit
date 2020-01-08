f4efaedd7db6d9d18664b0faf2be24daa111f51e
Manuel Fernandez <manferlo81@gmail.com>
2020-01-08 12:54:56 +0700

    chore: unit finder cleanup

--- src/create-unit-finder.ts
+++ src/create-unit-finder.ts
@@ -2,61 +2,50 @@ import { FindUnitFunction, FindUnitResult } from './formatter-types'
 import { pow } from './math'
 import { TableItem } from './types'
 
-function createUnitFinder(table?: TableItem[]): FindUnitFunction {
+function createModernUnitFinder(table: FindUnitResult[]): FindUnitFunction {
 
-  if (!table) {
+  return (value: number): FindUnitResult => {
 
-    return (value: number): FindUnitResult => {
-
-      const r0: FindUnitResult = { pre: '', div: 1 }
-
-      if (!value) {
-        return r0
-      }
-
-      const table2: FindUnitResult[] = [
-        { pre: 'T', div: 1e12 },
-        { pre: 'G', div: 1e9 },
-        { pre: 'M', div: 1e6 },
-        { pre: 'K', div: 1e3 },
-        r0,
-        { pre: 'm', div: 1e-3 },
-        { pre: '\u00b5', div: 1e-6 },
-        { pre: 'n', div: 1e-9 },
-        { pre: 'p', div: 1e-12 },
-        { pre: 'f', div: 1e-15 },
-      ]
-
-      const last = table2.length - 1
-      for (let i = 0; i < last; i++) {
-        const obj = table2[i]
-        if (value >= obj.div) {
-          return obj
-        }
-      }
-
-      return table2[last]
-
+    if (!value) {
+      return { pre: '', div: 1 }
     }
 
-  }
-
-  const find = (value: number): TableItem => {
-    const pp = value ? Math.floor(Math.log10(value)) : 0
     const last = table.length - 1
     for (let i = 0; i < last; i++) {
       const obj = table[i]
-      if (obj.power <= pp) {
+      if (value >= obj.div) {
         return obj
       }
     }
+
     return table[last]
+
   }
 
-  return (value: number): FindUnitResult => {
-    const { power, pre } = find(value)
-    return { div: pow(10, power), pre }
+}
+
+function createUnitFinder(table?: TableItem[]): FindUnitFunction {
+
+  if (!table) {
+    return createModernUnitFinder([
+      { pre: 'T', div: 1e12 },
+      { pre: 'G', div: 1e9 },
+      { pre: 'M', div: 1e6 },
+      { pre: 'K', div: 1e3 },
+      { pre: '', div: 1 },
+      { pre: 'm', div: 1e-3 },
+      { pre: '\u00b5', div: 1e-6 },
+      { pre: 'n', div: 1e-9 },
+      { pre: 'p', div: 1e-12 },
+      { pre: 'f', div: 1e-15 },
+    ])
   }
+
+  return createModernUnitFinder(
+    table.map<FindUnitResult>(
+      ({ pre, power }) => ({ pre, div: pow(10, power) }),
+    ).sort(({ div: div1 }, { div: div2 }) => (div2 - div1)),
+  )
 
 }
 
