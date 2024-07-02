e5b7f662c272fb201fceae35c8855e87ab2e35f9
Manuel Fernández <manferlo81@gmail.com>
2024-07-02 19:39:46 +0700

    feat: Allow parse with MICRO sign (/u00b5)

--- __test__/parse/unit-option.test.ts
+++ __test__/parse/unit-option.test.ts
@@ -1,4 +1,4 @@
-import { createParser } from '../../src';
+import { MICRO, createParser } from '../../src';
 
 describe('parse "unit" option', () => {
 
@@ -9,6 +9,7 @@ describe('parse "unit" option', () => {
     const values = [
       '10 x',
       '10 xg',
+      '10 gx',
       '10 mx',
       '10 Kx',
     ];
@@ -21,23 +22,26 @@ describe('parse "unit" option', () => {
 
   test('Should parse with correct unit', () => {
 
-    const parse = createParser({ unit: 'g' });
+    const unit = 'g';
+    const parse = createParser({ unit });
 
     const values = [
+      { value: '10p', expected: 10e-12 },
+      { value: '10n', expected: 10e-9 },
+      { value: '10u', expected: 10e-6 },
+      { value: `10${MICRO}`, expected: 10e-6 },
       { value: '10m', expected: 10e-3 },
       { value: '10', expected: 10 },
       { value: '10k', expected: 10e3 },
       { value: '10K', expected: 10e3 },
-      { value: '2M', expected: 2e6 },
-      { value: '2meg', expected: 2e6 },
-      { value: '10mg', expected: 10e-3 },
-      { value: '10g', expected: 10 },
-      { value: '10kg', expected: 10e3 },
-      { value: '10Kg', expected: 10e3 },
+      { value: '10M', expected: 10e6 },
+      { value: '10G', expected: 10e9 },
+      { value: '10T', expected: 10e12 },
     ];
 
     values.forEach(({ value, expected }) => {
-      expect(parse(value)).toBe(expected);
+      expect(parse(value)).toBeCloseTo(expected);
+      expect(parse(`${value}${unit}`)).toBeCloseTo(expected);
     });
 
   });
