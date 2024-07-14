104571cfa1b9f6e79cf156d7ad9a09e53d98da3c
Manuel Fernández <manferlo81@gmail.com>
2024-07-14 22:04:53 +0700

    chore(test): Add test

---
+++ __test__/common/transform-items.test.ts
@@ -0,0 +1,27 @@
+import { ExponentFindItems, MultiplierFindItem } from '../../src';
+import { transformFindItems } from '../../src/common/transform-items';
+
+describe('transformFindItems function', () => {
+
+  test('', () => {
+
+    const base = 10;
+    const items: ExponentFindItems = [
+      { pre: 'k', exp: 3 },
+      { pre: 'M', exp: 6 },
+      { pre: 'm', exp: -3 },
+      { pre: 'u', exp: -6 },
+    ];
+
+    const transformed = transformFindItems(items, base);
+    const expected = items.map<MultiplierFindItem>(({ pre, exp }) => {
+      const mul = base ** exp;
+      return { pre, mul };
+    });
+
+    expect(transformed.length).toBe(items.length);
+    expect(transformed).toEqual(expected);
+
+  });
+
+});
