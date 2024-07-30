b4ac4f1806b356aea85445f9cb91e20960726bb7
Manuel Fernández <manferlo81@gmail.com>
2024-07-30 23:16:08 +0700

    chore: Switch to eslint@9
    * Update eslint to v9.8.0
    * Install @eslint/js@9.8.0
    * Install @types/eslint__js@8.42.3
    * Install globals@15.8.0
    * Install typescript-eslint@7.18.0
    * Install @stylistic/eslint-plugin@2.4.0

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import globals from 'globals';
+import { config, configs as typescriptConfigs } from 'typescript-eslint';
+
+const rule = (options) => ['error', options];
+
+const pluginRules = (pluginName, rules) => Object.keys(rules).reduce((output, ruleName) => {
+  const pluginPrefixedRuleName = `${pluginName}/${ruleName}`;
+  const ruleEntry = rules[ruleName];
+  return { ...output, [pluginPrefixedRuleName]: ruleEntry };
+}, {});
+
+const eslintRules = {
+  'no-useless-rename': 'error',
+  'object-shorthand': 'error',
+};
+
+const typescriptRules = pluginRules('@typescript-eslint', {
+  'restrict-template-expressions': 'off',
+});
+
+const stylisticRules = pluginRules('@stylistic', {
+  'indent': rule(2),
+  'linebreak-style': rule('windows'),
+  'quotes': rule('single'),
+  'semi': rule('always'),
+
+  'comma-dangle': rule('always-multiline'),
+  'arrow-parens': rule('always'),
+  'member-delimiter-style': rule({}),
+  'no-extra-parens': 'error',
+
+  'no-multiple-empty-lines': rule({
+    max: 1,
+    maxBOF: 0,
+    maxEOF: 0,
+  }),
+
+  'no-floating-decimal': 'off',
+  'padded-blocks': 'off',
+
+});
+
+const rules = { ...eslintRules, ...typescriptRules, ...stylisticRules };
+
+const typescriptFlatConfigs = config(
+  ...typescriptConfigs.strictTypeChecked,
+  ...typescriptConfigs.stylisticTypeChecked,
+  { languageOptions: { parserOptions: { project: true } } },
+  { files: ['**/*.{js,mjs,cjs}'], ...typescriptConfigs.disableTypeChecked },
+);
+
+export default config(
+  { ignores: ['dist', 'coverage'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  js.configs.recommended,
+  ...typescriptFlatConfigs,
+  stylistic.configs['recommended-flat'],
+  { rules },
+);
