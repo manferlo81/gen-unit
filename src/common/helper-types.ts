5e334167dc7c0f7829456ce244c16a086d9e5903
Manuel Fernández <manferlo81@gmail.com>
2024-08-11 10:01:04 +0700

    chore(types): fix internal types

--- src/common/helper-types.ts
+++ src/common/helper-types.ts
@@ -1,14 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type Anything = any;
-
 // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
 type Void = void;
 
-export type AnyFunction = (this: Anything, ...args: Anything[]) => Anything;
-export type AnyArray = Anything[];
-
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-export type TypeCheckFunction<B> = <T extends B>(value: unknown) => value is T;
+export type TypeCheckFunction<T> = (value: unknown) => value is T;
 
 export type Nullish = null | undefined | Void;
 export type AllowNullish<T> = T | Nullish;
--- src/common/is.ts
+++ src/common/is.ts
@@ -1,28 +1,30 @@
-import type { AnyArray, AnyFunction, Nullish, TypeCheckFunction } from './helper-types';
-
-interface TypeOfMap {
-  number: number;
-  object: object;
-  function: AnyFunction;
-}
-
-type TypeOfResult = keyof TypeOfMap;
-
-function isType<K extends TypeOfResult>(type: K): TypeCheckFunction<TypeOfMap[K]> {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-  return <T extends TypeOfMap[K]>(value: unknown): value is T => {
-    return typeof value === type;
-  };
-}
+import type { Nullish, TypeCheckFunction } from './helper-types';
 
 export function isNullish(value: unknown): value is Nullish {
   return value == null;
 }
 
-export const isFunction = isType('function');
-
-export const isObject = isType('object');
-export const { isArray } = Array as { isArray: TypeCheckFunction<AnyArray> };
+function isType(type: 'number'): TypeCheckFunction<number>;
+function isType(type: 'object'): TypeCheckFunction<object>;
+function isType(type: 'function'): TypeCheckFunction<CallableFunction>;
+function isType(type: string) {
+  return (value: unknown) => {
+    return typeof value === type;
+  };
+}
 
 export const isNumber = isType('number');
-export const { isFinite: isFiniteNumber } = Number;
+export const isObject = isType('object');
+export const isFunction = isType('function');
+
+interface ExtendedArrayConstructor extends ArrayConstructor {
+  isArray: TypeCheckFunction<unknown[]>;
+}
+
+export const { isArray } = Array as ExtendedArrayConstructor;
+
+interface ExtendedNumberConstructor extends NumberConstructor {
+  isFinite: TypeCheckFunction<number>;
+}
+
+export const { isFinite: isFiniteNumber } = Number as ExtendedNumberConstructor;
